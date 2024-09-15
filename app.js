f1eccd947cdd3cbe55560e7dcd68fb6bf4546eb0
bteicho@gmail.com <btegicho@gmail.com>
2024-09-15 22:37:07 +0200

    folder and file created, wecomeview created

---
+++ app.js
@@ -0,0 +1,5 @@
+import {createWelcomePageView} from './src/views/welcomePageView.js';
+const loadApp = () => {
+  createWelcomePageView(); // This will create and append the welcome page to the DOM
+};
+window.addEventListener('load', loadApp);
---
+++ src/views/welcomePageView.js
@@ -0,0 +1,22 @@
+
+export const createWelcomePageView = () => {
+
+const userInterface = document.getElementById('user-interface');
+const welcomePageView = document.createElement('div');
+welcomePageView.innerHTML = `
+<div class="welcome-page">
+<h1> country Information Look Up</h1>
+
+<input type ='text' id ='search' placeholder = 'Enter a country name'>
+<button id = 'search-btn'>Search </button>
+</div>
+`
+
+
+userInterface.appendChild(welcomePageView); 
+
+document.getElementById('search-btn').addEventListener('click',searchCountry);
+
+}
+
+
