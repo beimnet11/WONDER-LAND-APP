8a384ef024fb8ec5028f47dd55cce8f6ef3851e8
bteicho@gmail.com <btegicho@gmail.com>
2024-09-17 12:47:54 +0200

    styling inside css

--- src/pages/fetchNewsPage.js
+++ src/pages/fetchNewsPage.js
@@ -1,4 +1,5 @@
 
+
 export const fetchNewsData = async (countryName) => {
   const apiKey = '010bcef794cd4070b34f796b6b110317'; // Replace with your NewsAPI key
   const newsData = await fetch(`https://newsapi.org/v2/everything?q=${countryName}&apiKey=${apiKey}`);
